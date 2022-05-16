0d6e1bcadfb09492aed5b9010045cf501c3ee27d
Sofia Leon <sofialeon@google.com>
2022-05-16 13:18:00 -0700

    feat!: Update library to use Node 12

--- protos/protos.js
+++ protos/protos.js
@@ -7935,7 +7935,7 @@
                         };
     
                         /**
-                         * Callback as used by {@link google.cloud.networkmanagement.v1.ReachabilityService#listConnectivityTests}.
+                         * Callback as used by {@link google.cloud.networkmanagement.v1.ReachabilityService|listConnectivityTests}.
                          * @memberof google.cloud.networkmanagement.v1.ReachabilityService
                          * @typedef ListConnectivityTestsCallback
                          * @type {function}
@@ -7968,7 +7968,7 @@
                          */
     
                         /**
-                         * Callback as used by {@link google.cloud.networkmanagement.v1.ReachabilityService#getConnectivityTest}.
+                         * Callback as used by {@link google.cloud.networkmanagement.v1.ReachabilityService|getConnectivityTest}.
                          * @memberof google.cloud.networkmanagement.v1.ReachabilityService
                          * @typedef GetConnectivityTestCallback
                          * @type {function}
@@ -8001,7 +8001,7 @@
                          */
     
                         /**
-                         * Callback as used by {@link google.cloud.networkmanagement.v1.ReachabilityService#createConnectivityTest}.
+                         * Callback as used by {@link google.cloud.networkmanagement.v1.ReachabilityService|createConnectivityTest}.
                          * @memberof google.cloud.networkmanagement.v1.ReachabilityService
                          * @typedef CreateConnectivityTestCallback
                          * @type {function}
@@ -8034,7 +8034,7 @@
                          */
     
                         /**
-                         * Callback as used by {@link google.cloud.networkmanagement.v1.ReachabilityService#updateConnectivityTest}.
+                         * Callback as used by {@link google.cloud.networkmanagement.v1.ReachabilityService|updateConnectivityTest}.
                          * @memberof google.cloud.networkmanagement.v1.ReachabilityService
                          * @typedef UpdateConnectivityTestCallback
                          * @type {function}
@@ -8067,7 +8067,7 @@
                          */
     
                         /**
-                         * Callback as used by {@link google.cloud.networkmanagement.v1.ReachabilityService#rerunConnectivityTest}.
+                         * Callback as used by {@link google.cloud.networkmanagement.v1.ReachabilityService|rerunConnectivityTest}.
                          * @memberof google.cloud.networkmanagement.v1.ReachabilityService
                          * @typedef RerunConnectivityTestCallback
                          * @type {function}
@@ -8100,7 +8100,7 @@
                          */
     
                         /**
-                         * Callback as used by {@link google.cloud.networkmanagement.v1.ReachabilityService#deleteConnectivityTest}.
+                         * Callback as used by {@link google.cloud.networkmanagement.v1.ReachabilityService|deleteConnectivityTest}.
                          * @memberof google.cloud.networkmanagement.v1.ReachabilityService
                          * @typedef DeleteConnectivityTestCallback
                          * @type {function}
@@ -18813,7 +18813,7 @@
                         };
     
                         /**
-                         * Callback as used by {@link google.cloud.networkmanagement.v1beta1.ReachabilityService#listConnectivityTests}.
+                         * Callback as used by {@link google.cloud.networkmanagement.v1beta1.ReachabilityService|listConnectivityTests}.
                          * @memberof google.cloud.networkmanagement.v1beta1.ReachabilityService
                          * @typedef ListConnectivityTestsCallback
                          * @type {function}
@@ -18846,7 +18846,7 @@
                          */
     
                         /**
-                         * Callback as used by {@link google.cloud.networkmanagement.v1beta1.ReachabilityService#getConnectivityTest}.
+                         * Callback as used by {@link google.cloud.networkmanagement.v1beta1.ReachabilityService|getConnectivityTest}.
                          * @memberof google.cloud.networkmanagement.v1beta1.ReachabilityService
                          * @typedef GetConnectivityTestCallback
                          * @type {function}
@@ -18879,7 +18879,7 @@
                          */
     
                         /**
-                         * Callback as used by {@link google.cloud.networkmanagement.v1beta1.ReachabilityService#createConnectivityTest}.
+                         * Callback as used by {@link google.cloud.networkmanagement.v1beta1.ReachabilityService|createConnectivityTest}.
                          * @memberof google.cloud.networkmanagement.v1beta1.ReachabilityService
                          * @typedef CreateConnectivityTestCallback
                          * @type {function}
@@ -18912,7 +18912,7 @@
                          */
     
                         /**
-                         * Callback as used by {@link google.cloud.networkmanagement.v1beta1.ReachabilityService#updateConnectivityTest}.
+                         * Callback as used by {@link google.cloud.networkmanagement.v1beta1.ReachabilityService|updateConnectivityTest}.
                          * @memberof google.cloud.networkmanagement.v1beta1.ReachabilityService
                          * @typedef UpdateConnectivityTestCallback
                          * @type {function}
@@ -18945,7 +18945,7 @@
                          */
     
                         /**
-                         * Callback as used by {@link google.cloud.networkmanagement.v1beta1.ReachabilityService#rerunConnectivityTest}.
+                         * Callback as used by {@link google.cloud.networkmanagement.v1beta1.ReachabilityService|rerunConnectivityTest}.
                          * @memberof google.cloud.networkmanagement.v1beta1.ReachabilityService
                          * @typedef RerunConnectivityTestCallback
                          * @type {function}
@@ -18978,7 +18978,7 @@
                          */
     
                         /**
-                         * Callback as used by {@link google.cloud.networkmanagement.v1beta1.ReachabilityService#deleteConnectivityTest}.
+                         * Callback as used by {@link google.cloud.networkmanagement.v1beta1.ReachabilityService|deleteConnectivityTest}.
                          * @memberof google.cloud.networkmanagement.v1beta1.ReachabilityService
                          * @typedef DeleteConnectivityTestCallback
                          * @type {function}
@@ -27719,6 +27719,7 @@
                  * @property {boolean|null} [packed] FieldOptions packed
                  * @property {google.protobuf.FieldOptions.JSType|null} [jstype] FieldOptions jstype
                  * @property {boolean|null} [lazy] FieldOptions lazy
+                 * @property {boolean|null} [unverifiedLazy] FieldOptions unverifiedLazy
                  * @property {boolean|null} [deprecated] FieldOptions deprecated
                  * @property {boolean|null} [weak] FieldOptions weak
                  * @property {Array.<google.protobuf.IUninterpretedOption>|null} [uninterpretedOption] FieldOptions uninterpretedOption
@@ -27774,6 +27775,14 @@
                  * @instance
                  */
                 FieldOptions.prototype.lazy = false;
+    
+                /**
+                 * FieldOptions unverifiedLazy.
+                 * @member {boolean} unverifiedLazy
+                 * @memberof google.protobuf.FieldOptions
+                 * @instance
+                 */
+                FieldOptions.prototype.unverifiedLazy = false;
     
                 /**
                  * FieldOptions deprecated.
@@ -27851,6 +27860,8 @@
                         writer.uint32(/* id 6, wireType 0 =*/48).int32(message.jstype);
                     if (message.weak != null && Object.hasOwnProperty.call(message, "weak"))
                         writer.uint32(/* id 10, wireType 0 =*/80).bool(message.weak);
+                    if (message.unverifiedLazy != null && Object.hasOwnProperty.call(message, "unverifiedLazy"))
+                        writer.uint32(/* id 15, wireType 0 =*/120).bool(message.unverifiedLazy);
                     if (message.uninterpretedOption != null && message.uninterpretedOption.length)
                         for (var i = 0; i < message.uninterpretedOption.length; ++i)
                             $root.google.protobuf.UninterpretedOption.encode(message.uninterpretedOption[i], writer.uint32(/* id 999, wireType 2 =*/7994).fork()).ldelim();
@@ -27907,6 +27918,9 @@
                             break;
                         case 5:
                             message.lazy = reader.bool();
+                            break;
+                        case 15:
+                            message.unverifiedLazy = reader.bool();
                             break;
                         case 3:
                             message.deprecated = reader.bool();
@@ -27991,6 +28005,9 @@
                     if (message.lazy != null && message.hasOwnProperty("lazy"))
                         if (typeof message.lazy !== "boolean")
                             return "lazy: boolean expected";
+                    if (message.unverifiedLazy != null && message.hasOwnProperty("unverifiedLazy"))
+                        if (typeof message.unverifiedLazy !== "boolean")
+                            return "unverifiedLazy: boolean expected";
                     if (message.deprecated != null && message.hasOwnProperty("deprecated"))
                         if (typeof message.deprecated !== "boolean")
                             return "deprecated: boolean expected";
@@ -28076,6 +28093,8 @@
                     }
                     if (object.lazy != null)
                         message.lazy = Boolean(object.lazy);
+                    if (object.unverifiedLazy != null)
+                        message.unverifiedLazy = Boolean(object.unverifiedLazy);
                     if (object.deprecated != null)
                         message.deprecated = Boolean(object.deprecated);
                     if (object.weak != null)
@@ -28163,6 +28182,7 @@
                         object.lazy = false;
                         object.jstype = options.enums === String ? "JS_NORMAL" : 0;
                         object.weak = false;
+                        object.unverifiedLazy = false;
                         object[".google.api.resourceReference"] = null;
                     }
                     if (message.ctype != null && message.hasOwnProperty("ctype"))
@@ -28177,6 +28197,8 @@
                         object.jstype = options.enums === String ? $root.google.protobuf.FieldOptions.JSType[message.jstype] : message.jstype;
                     if (message.weak != null && message.hasOwnProperty("weak"))
                         object.weak = message.weak;
+                    if (message.unverifiedLazy != null && message.hasOwnProperty("unverifiedLazy"))
+                        object.unverifiedLazy = message.unverifiedLazy;
                     if (message.uninterpretedOption && message.uninterpretedOption.length) {
                         object.uninterpretedOption = [];
                         for (var j = 0; j < message.uninterpretedOption.length; ++j)
@@ -32555,7 +32577,7 @@
                 };
     
                 /**
-                 * Callback as used by {@link google.longrunning.Operations#listOperations}.
+                 * Callback as used by {@link google.longrunning.Operations|listOperations}.
                  * @memberof google.longrunning.Operations
                  * @typedef ListOperationsCallback
                  * @type {function}
@@ -32588,7 +32610,7 @@
                  */
     
                 /**
-                 * Callback as used by {@link google.longrunning.Operations#getOperation}.
+                 * Callback as used by {@link google.longrunning.Operations|getOperation}.
                  * @memberof google.longrunning.Operations
                  * @typedef GetOperationCallback
                  * @type {function}
@@ -32621,7 +32643,7 @@
                  */
     
                 /**
-                 * Callback as used by {@link google.longrunning.Operations#deleteOperation}.
+                 * Callback as used by {@link google.longrunning.Operations|deleteOperation}.
                  * @memberof google.longrunning.Operations
                  * @typedef DeleteOperationCallback
                  * @type {function}
@@ -32654,7 +32676,7 @@
                  */
     
                 /**
-                 * Callback as used by {@link google.longrunning.Operations#cancelOperation}.
+                 * Callback as used by {@link google.longrunning.Operations|cancelOperation}.
                  * @memberof google.longrunning.Operations
                  * @typedef CancelOperationCallback
                  * @type {function}
@@ -32687,7 +32709,7 @@
                  */
     
                 /**
-                 * Callback as used by {@link google.longrunning.Operations#waitOperation}.
+                 * Callback as used by {@link google.longrunning.Operations|waitOperation}.
                  * @memberof google.longrunning.Operations
                  * @typedef WaitOperationCallback
                  * @type {function}
